1a0f3a12092e8ae4dd3be8d7fa82a22f086735cd
M-KayMuzik <makehitzndesignz@gmail.com>
2015-01-10 20:29:05 -0500

    molly: Updated rootdir contents
    * Android.mk edited
    * rootdir/ect files replaced for molly

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,40 +1,33 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.mojo
+LOCAL_MODULE       := fstab.molly
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.mojo
+LOCAL_SRC_FILES    := etc/fstab.molly
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.mojo.rc
+LOCAL_MODULE       := init.molly.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.mojo.rc
+LOCAL_SRC_FILES    := etc/init.molly.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.mojo.usb.rc
+LOCAL_MODULE       := init.molly.usb.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.mojo.usb.rc
+LOCAL_SRC_FILES    := etc/init.molly.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := power.mojo.rc
+LOCAL_MODULE       := ueventd.molly.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/power.mojo.rc
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := ueventd.mojo.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/ueventd.mojo.rc
+LOCAL_SRC_FILES    := etc/ueventd.molly.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
