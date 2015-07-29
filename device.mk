b4bacbbd2f6dee9380d0673cc1eb1d795874afea
Ronghua Wu <ronghuawu@google.com>
2015-07-28 18:45:30 -0700

    molly: add media_codecs_performance.xml
    Bug: 22504214
    Change-Id: I2df1984033f78c835a1997baffc77b8a7142b066

--- device.mk
+++ device.mk
@@ -68,6 +68,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_tv.xml:system/etc/media_codecs_google_tv.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml
 
 
