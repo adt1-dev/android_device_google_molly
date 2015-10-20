3b976c534e1577bbcbbb4694ae73f1bf64d56a7d
r3pwn (Greg Willard) <wickett06@gmail.com>
2015-10-20 00:39:07 -0400

    molly: alphabetize device.mk Yes, if you're curious, I'm bored.

--- device.mk
+++ device.mk
@@ -25,79 +25,43 @@ $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-he
 
 $(call inherit-product-if-exists, vendor/google/molly/molly-vendor.mk)
 
-# Overlay
-DEVICE_PACKAGE_OVERLAYS += \
-    device/google/molly/overlay
+DEVICE_PACKAGE_OVERLAYS += device/google/molly/overlay
 
-# Include the Tv parts for use
-PRODUCT_PACKAGES += \
-    TvProvider \
-    TvSettings \
-    tv_input.default
 
-# Include AppDrawer from the Nexus Player
-PRODUCT_PACKAGES += \
-    AppDrawer
-
-# Set the screen density to be the same as stock
-PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=320
-
-# Ramdisk
-PRODUCT_PACKAGES += \
-    fstab.molly \
-    init.molly.rc \
-    init.molly.usb.rc \
-    ueventd.molly.rc
-
-# Permissions
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
-    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
-    frameworks/native/data/etc/android.hardware.hdmi.cec.xml:system/etc/permissions/android.hardware.hdmi.cec.xml \
-    frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    $(LOCAL_PATH)/permissions/com.nvidia.nvsi.xml:system/etc/permissions/com.nvidia.nvsi.xml \
-    $(LOCAL_PATH)/permissions/tv_core_hardware.xml:system/etc/permissions/tv_core_hardware.xml \
-    $(LOCAL_PATH)/permissions/molly_hardware.xml:system/etc/permissions/molly_hardware.xml
+# ADB Over Wifi
+PRODUCT_PROPERTY_OVERRIDES += \
+    service.adb.tcp.port=4321
 
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/libaudio/audio_policy.conf:system/etc/audio_policy.conf
-
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.primary.molly \
     audio.r_submix.default \
     audio.usb.default \
     libtinyalsa
-
 USE_CUSTOM_AUDIO_POLICY := 1
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.cache-params=10240/20480/15 \
+    media.aac_51_output_enabled=true \
+    persist.sys.media.avsync=true
 
-# Hdmi CEC: molly works as a playback device (4).
-PRODUCT_PROPERTY_OVERRIDES += ro.hdmi.device_type=4
-
-# Media config
+# Codec Configs
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml
 
-# Wifi
-PRODUCT_PACKAGES += \
-    dhcpcd.conf \
-    hostapd \
-    libwpa_client \
-    wpa_supplicant \
-    wpa_supplicant.conf
 
-# Automagically fix wifi
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true \
+    ro.com.widevine.cachesize=16777216
+
+# HDMI
+PRODUCT_PROPERTY_OVERRIDES += ro.hdmi.device_type=4
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
@@ -114,33 +78,54 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayouts/Vendor_20a0_Product_0001.kl:system/usr/keylayout/Vendor_20a0_Product_0001.kl \
     $(LOCAL_PATH)/keylayouts/Vendor_18d1_Product_2c40.kl:system/usr/keylayout/Vendor_18d1_Product_2c40.kl
 
-# we have enough storage space to hold precise GC data
+# Misc
 PRODUCT_TAGS += dalvik.gc.type-precise
-
 PRODUCT_CHARACTERISTICS := tv,nosdcard
+PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=320
 
-# Set default USB interface
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-     persist.sys.usb.config=mtp
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
+    frameworks/native/data/etc/android.hardware.hdmi.cec.xml:system/etc/permissions/android.hardware.hdmi.cec.xml \
+    frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    $(LOCAL_PATH)/permissions/com.nvidia.nvsi.xml:system/etc/permissions/com.nvidia.nvsi.xml \
+    $(LOCAL_PATH)/permissions/tv_core_hardware.xml:system/etc/permissions/tv_core_hardware.xml \
+    $(LOCAL_PATH)/permissions/molly_hardware.xml:system/etc/permissions/molly_hardware.xml
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.molly \
+    init.molly.rc \
+    init.molly.usb.rc \
+    ueventd.molly.rc
+
+# TV-specific Apps/Packages
+PRODUCT_PACKAGES += \
+    TvProvider \
+    TvSettings \
+    tv_input.default \
+    AppDrawer
 
 # USB
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+     persist.sys.usb.config=mtp
 PRODUCT_PACKAGES += \
     com.android.future.usb.accessory
 
 # Wifi
+PRODUCT_PACKAGES += \
+    dhcpcd.conf \
+    hostapd \
+    libwpa_client \
+    wpa_supplicant \
+    wpa_supplicant.conf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
 PRODUCT_PROPERTY_OVERRIDES += wifi.interface=wlan0
 
-# ADB Over Wifi
-PRODUCT_PROPERTY_OVERRIDES += \
-    service.adb.tcp.port=4321
-
-# Audio
-PRODUCT_PROPERTY_OVERRIDES += \
-    media.stagefright.cache-params=10240/20480/15 \
-    media.aac_51_output_enabled=true \
-    persist.sys.media.avsync=true
-
-# Enable Widevine drm
-PRODUCT_PROPERTY_OVERRIDES += \
-    drm.service.enabled=true \
-    ro.com.widevine.cachesize=16777216
