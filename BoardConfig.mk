8202a646ce1ef15b612ddd031a45ba38428c6a90
Greg Willard <Wickett06@gmail.com>
2015-10-26 08:45:59 -0400

    molly: enable WITH_DEXPREOPT for linux hosts
    This should speed up the first boot a bit.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,14 @@
 
 TARGET_SPECIFIC_HEADER_PATH := device/google/molly/include
 
+# Enable dex-preoptimization to speed up first boot sequence
+ifeq ($(HOST_OS),linux)
+  ifeq ($(TARGET_BUILD_VARIANT),user)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
 
 # Audio
 BOARD_USES_ALSA_AUDIO := true
